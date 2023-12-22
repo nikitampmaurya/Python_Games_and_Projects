d97db9067b9006ca79e64654626835b87f95f559
Nikita <132465691+nikitampmaurya@users.noreply.github.com>
2023-12-22 14:21:25 +0530

    Update brainteaser.py

--- brainteaser.py
+++ brainteaser.py
@@ -7,50 +7,53 @@ because player is expected to choose the colour of the font.
 import random
 from colorama import Fore, Style
 
-# List of words
-word_list = ["green","white","red","purple","blue","yellow"]
-
-# List of colors
-color_list = [Fore.GREEN, Fore.WHITE, Fore.RED, Fore.MAGENTA, Fore.BLUE, Fore.YELLOW]
-rounds = 0
-score = 0
-while rounds <10:
+def select_random_numbers():
     while True:
         num1 = random.randint(0, 5)
         num2 = random.randint(0, 5)
         num3 = random.randint(0, 5)
         if num1 != num2 and num1 != num3 and num2 != num3 and num1 != num2 != num3:
             break
-    #print("num1: ",num1,"num2: ",num2,"num3: ", num3)
-# Select a random word and a random color
+    return num1, num2, num3
+
+def print_random_word_and_options(word_list, color_list, num1, num2, num3):
     random_word = word_list[num1]
-# Select a random word and a random font
     random_color = color_list[num2]
 
-# Print the word in the selected color
     print(random_color + random_word + Style.RESET_ALL)
-#preparing question, we need to shuffle the two words in the questions
+
     list = [word_list[num3], word_list[num2]]
     random.shuffle(list)
     print(list[1], "or", list[0])
-    ans = input("a or b: ")
-    new_list = {"a":list[1], "b":list[0]}
-#print(new_list)
-#print("answer: ",ans)
-#print("word_list[num2]: ",word_list[num2])
-    if ans == "a":
-        if new_list[ans] == word_list[num2]:
-             print("correct")
-             score +=1
+
+    return list, num2
+
+def play_game():
+    word_list = ["green", "white", "red", "purple", "blue", "yellow"]
+    color_list = [Fore.GREEN, Fore.WHITE, Fore.RED, Fore.MAGENTA, Fore.BLUE, Fore.YELLOW]
+    rounds = 0
+    score = 0
+
+    while rounds < 10:
+        num1, num2, num3 = select_random_numbers()
+
+        word_options, selected_color = print_random_word_and_options(word_list, color_list, num1, num2, num3)
+
+        ans = input("a or b: ")
+        new_list = {"a": word_options[1], "b": word_options[0]}
+
+        if ans == "a" or ans == "b":
+            if new_list[ans] == word_list[selected_color]:
+                print("correct")
+                score += 1
+            else:
+                print("wrong")
         else:
-             print("wrong")
-    else:
-        if ans == "b":
-             if new_list[ans] == word_list[num2]:
-                  print("correct")
-                  score +=1
-             else:
-                 print("wrong")
-    rounds +=1
-#print("Number of rounds remaining: ",rounds)
-print("your score: ",score,"/",rounds)
+            print("Invalid input! Please choose 'a' or 'b'.")
+
+        rounds += 1
+
+    print("Your score:", score, "/", rounds)
+
+# Run the game
+play_game()
