53687fb5a47399e45fdfc0e6d96788af9673065c
Nikita <132465691+nikitampmaurya@users.noreply.github.com>
2024-01-12 17:30:47 +0530

    Update brainteaser.py

--- brainteaser.py
+++ brainteaser.py
@@ -1,23 +1,24 @@
 """
-Description: Player must write the font colour of the given word. The game will have 10 rounds back to back.
+Description: This game will have 10 rounds back to back. You will be presented with one word and two options. 
+You must write the font colour of the given word. 
 For example: the given word is "white", then given option are "white" or "black". Here, the correct answer is "black", 
 because player is expected to choose the colour of the font.  
 """
 
-import random
-from colorama import Fore, Style
+import random  #import random module to generate random numbers 
+from colorama import Fore, Style  #import colours 
 
-def select_random_numbers():
-    while True:
-        num1 = random.randint(0, 5)
-        num2 = random.randint(0, 5)
+def select_random_numbers():               #we need three random numbers
+    while True:                            #loop will go on until we get three randomly selected distinct numbers
+        num1 = random.randint(0, 5)        #num1 to randomly select a word from the word list
+        num2 = random.randint(0, 5)        #num2 to randomly select a colour from the color list
         num3 = random.randint(0, 5)
         if num1 != num2 and num1 != num3 and num2 != num3 and num1 != num2 != num3:
             break
     return num1, num2, num3
 
-def print_random_word_and_options(word_list, color_list, num1, num2, num3):
-    random_word = word_list[num1]
+def print_random_word_and_options(word_list, color_list, num1, num2, num3):     
+    random_word = word_list[num1]                                                
     random_color = color_list[num2]
 
     print(random_color + random_word + Style.RESET_ALL)
