806d413d6b2addaff48833a563a8376bb2e166a7
Nikita <132465691+nikitampmaurya@users.noreply.github.com>
2024-02-06 12:58:52 +0530

    Update ATM(OOP).py

--- ATM(OOP).py
+++ ATM(OOP).py
@@ -1,9 +1,30 @@
 class Atm: #name of class should always be in PascalCase
     def __init__(self): #Constructor - assign value to data
-        self.pin = ""
-        self.balance = 0
-        self.menu() #call a method to do something
-    def menu(self): #method
+        self.__pin = ""
+        self.__balance = 0
+        self.__menu() #call a method to do something
+        
+    def get_pin(self):
+        return self.__pin
+    
+    def set_pin(self,new_pin):
+        if type(new_pin) == int:
+            self.__pin = new_pin
+            print("Pin has changed")
+        else:
+            print("not allowed")
+            
+    def get_balance(self):
+        return self.__balance
+    
+    def set_balance(self,new_balance):
+        if type(new_balance) == int:
+            self.__balance = new_balance
+            print("Balance has changed")
+        else:
+            print("not allowed")
+
+    def __menu(self): #method
         #taking input from users
         user_input = input(""" 
                            Hello, How would you like to proceed?
@@ -30,7 +51,7 @@ class Atm: #name of class should always be in PascalCase
         while True:
             temp=input("Enter your pin: ")
             if temp.isdigit():
-                self.pin = temp
+                self.__pin = temp
                 print("Pin is set successfully")
                 break
             elif not temp.isdigit():
@@ -38,18 +59,18 @@ class Atm: #name of class should always be in PascalCase
 
     def deposit(self):
         temp=input("Enter your pin to deposit amount: ")
-        if temp==self.pin:
+        if temp==self.__pin:
             amount = int(input("Enter the amount to deposit: "))
-            self.balance+=amount #updating initial value of balance from the constructor
+            self.__balance+=amount #updating initial value of balance from the constructor
             print("Deposit is successful")
         else:
             print("pin is wrong")
     def withdraw(self):
         temp=input("Enter your pin to withdraw amount: ")
-        if temp==self.pin:
+        if temp==self.__pin:
             amount = int(input("Enter the amount to withdraw: "))
-            if amount <= self.balance:
-                self.balance-=amount
+            if amount <= self.__balance:
+                self.__balance-=amount
                 print("Transaction was successful")
             else:
                 print("Insufficient Fund")
@@ -58,12 +79,10 @@ class Atm: #name of class should always be in PascalCase
 
     def check_balance(self):
         temp=input("Enter your pin to check balance: ")
-        if temp==self.pin:
-            print(self.balance)
+        if temp==self.__pin:
+            print(self.__balance)
         else:
             print("pin is wrong")
 
 sbi = Atm()
-sbi.deposit()
-sbi.withdraw()
-sbi.check_balance()
+
